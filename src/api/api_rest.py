8f0ff3a6384bc915b57e9308b6c9ae1267834f1c
cydonia-io <59589689+cydonia-io@users.noreply.github.com>
2020-06-01 16:04:23 -0500

    Update api_rest.py
    Remove api key for security

--- src/api/api_rest.py
+++ src/api/api_rest.py
@@ -7,5 +7,5 @@ Created on Sun May 31 00:36:04 2020
 """
 
 #API Rest Cryptowatch list
-api_price_bitmex_btcusd = "https://api.cryptowat.ch/markets/bitmex/btcusd-perpetual-futures/price?apikey=M1SR7D1BZ1Q688SYH30V"
+api_price_bitmex_btcusd = "https://api.cryptowat.ch/markets/bitmex/btcusd-perpetual-futures/price"
 
